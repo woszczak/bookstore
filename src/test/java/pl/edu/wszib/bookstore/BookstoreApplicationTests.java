b44671477438280b012abcb856a9830de862db3f
Justyna Woszczak <justynawoszczak1@gmail.com>
2023-06-11 21:24:49 +0200

    Initial commit

---
+++ src/test/java/pl/edu/wszib/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
